c4adcea1c446d51c77dfea1eeca147ffad822a16
nhatt <nhattuan44t@gmail.com>
2024-09-29 19:09:45 +0700

    firstProject

---
+++ src/main/java/com/toiec/toiec/ToiecApplication.java
@@ -0,0 +1,13 @@
+package com.toiec.toiec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToiecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToiecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toiec/toiec/ToiecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toiec.toiec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToiecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
